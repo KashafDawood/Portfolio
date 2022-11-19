81ea7db1a2ea3c65e2de54dee57c4baf664943de
kashafdawood <kashaf.dawood@gmail.com>
2022-11-19 22:24:20 +0500

    slider

--- script.js
+++ script.js
@@ -7,27 +7,43 @@ const menuIcon = document.getElementById("menuIcon");
 
 navContainer.style.right = "-767px";
 
-menuBtn.onclick = function(){
+menuBtn.onclick = function () {
 
-    if(navContainer.style.right === "-767px"){
+    if (navContainer.style.right === "-767px") {
         navContainer.style.right = "0"
-        menuIcon.classList.add("uil-multiply");
-        menuIcon.classList.remove("uil-bars");
+        menuIcon.classList.add("bx-x");
+        menuIcon.classList.remove("bx-menu");
     }
-    else{
+    else {
         navContainer.style.right = "-767px";
-        menuIcon.classList.remove("uil-multiply");
-        menuIcon.classList.add("uil-bars");
+        menuIcon.classList.remove("bx-x");
+        menuIcon.classList.add("bx-menu");
     }
 }
 
 // ========when click on navlink it will hide the navbar========
 const navLink = Array.from(document.getElementsByClassName("navLink"));
 
-for(let i = 0; i < navLink.length; i++){
-    navLink[i].addEventListener("click", function(){
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener("click", function () {
         navContainer.style.right = "-767px";
-        menuIcon.classList.remove("uil-multiply");
-        menuIcon.classList.add("uil-bars");
-    }); 
+        menuIcon.classList.remove("bx-x");
+        menuIcon.classList.add("bx-menu");
+    });
 }
+
+// ==============Slider=============
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grapCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
